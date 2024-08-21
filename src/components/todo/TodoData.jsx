70e1c2187cfff2dddc2eb1420760a33f4440da1b
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-08-21 22:29:08 +0700

    divide into components

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,10 @@
+const TodoData = () => {
+  return (
+    <div className="todo-data">
+      <p>Learn React</p>
+      <p>Watching Youtube</p>
+    </div>
+  );
+}
+
+export default TodoData;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+  return (
+    <div className="todo-new">
+      <input type="text" placeholder="Enter Text" />
+      <button>App</button>
+    </div>
+  );
+}
+
+export default TodoNew;

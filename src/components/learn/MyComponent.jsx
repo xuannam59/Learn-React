1034f744530c8f822fd03107156b67bae469ca2d
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-08-21 18:32:57 +0700

    use parameter in the React

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,9 +4,16 @@
 import "./style.css"
 
 const MyComponent = () => {
+  // const xuanNam = 12;// number
+  // const xuanNam = "Hello"; // String
+  const xuanNam = [1, 2, 3];
+  // const xuanNam = {
+  //   name: "Xuan Nam",
+  //   age: "22"
+  // }
   return (
     <>
-      <div>Xuan Nam & update</div>
+      <div>{JSON.stringify(xuanNam)} Xuan Nam & update</div>
       <div
         style={{ borderRadius: "3px" }}
         className="child"

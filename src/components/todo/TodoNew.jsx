da6243e3e021049ba7b471cb7f826596c4d391a8
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-08-22 16:02:27 +0700

    DOM Event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,23 @@
 const TodoNew = (props) => {
   const { addNewTodo } = props
+  // addNewTodo("Xuan Nam");
+  const handleClick = () => {
+    alert(`Click me `);
+  }
 
-  addNewTodo("Xuan Nam");
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
   return (
     <div className="todo-new">
-      <input type="text" placeholder="Enter Text" />
-      <button>App</button>
+      <input
+        type="text"
+        placeholder="Enter Text"
+        onChange={handleChange}
+      />
+      <button
+        onClick={handleClick}
+      >App</button>
     </div>
   );
 }

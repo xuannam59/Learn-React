fe3d8a9749949d1660e914f8912869b4f8caaa67
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-08-27 19:25:10 +0700

    create a new user

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,27 +1,60 @@
 import { Button, Input } from 'antd';
+import { useState } from 'react';
+import axios from 'axios';
 
 const UserForm = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleClick = () => {
+    const URL_BACKEND = 'http://localhost:8080/api/v1/user';
+    const data = {
+      fullName: fullName,
+      email: email,
+      password: password,
+      phone: phone
+    }
+    axios.post(URL_BACKEND, data);
+  }
+
   return (
     <div className='user-form' style={{ margin: "20px 0" }}>
       <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
         <div>
           <span>Full Name</span>
-          <Input />
+          <Input
+            value={fullName}
+            onChange={(event) => { setFullName(event.target.value) }}
+          />
         </div>
         <div>
           <span>Email</span>
-          <Input />
+          <Input
+            value={email}
+            onChange={(event) => { setEmail(event.target.value) }}
+          />
         </div>
         <div>
           <span>Password</span>
-          <Input.Password />
+          <Input.Password
+            value={password}
+            onChange={(event) => { setPassword(event.target.value) }}
+          />
         </div>
         <div>
           <span>Phone number</span>
-          <Input />
+          <Input
+            value={phone}
+            onChange={(event) => { setPhone(event.target.value) }}
+          />
         </div>
         <div>
-          <Button type="primary">Create User</Button>
+          <Button
+            type="primary"
+            onClick={handleClick}
+          >Create User</Button>
         </div>
       </div>
     </div>

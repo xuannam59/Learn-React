a2d0cbea573f60dfc6fbc7dfdcb8b83c6b1f220b
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-08-27 15:32:13 +0700

    created the user base component

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,6 +1,14 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
+
 const UserPage = () => {
   return (
-    <div>User page</div>
+    <div>
+      <div style={{ padding: "20px" }} >
+        <UserForm />
+        <UserTable />
+      </div>
+    </div >
   );
 }
 

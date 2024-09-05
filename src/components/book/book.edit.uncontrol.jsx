d303f84f0dfdb92974a5874e6d812d51c50f057e
Xuan Nam <79888762+Sukuna59@users.noreply.github.com>
2024-09-05 14:57:14 +0700

    update book update feature (UnControl)

---
+++ src/components/book/book.edit.uncontrol.jsx
@@ -0,0 +1,249 @@
+import { Form, Input, InputNumber, Modal, notification, Select } from "antd";
+import { useEffect, useState } from "react";
+import { handleUploadFile, updateBookApi } from "../../services/api.service";
+
+const EditBookUnControl = (props) => {
+    const {
+        loadBook,
+        dataUpdate, setDataUpdate,
+        isModalEditOpen, setIsModalEditOpen
+    } = props;
+    const [form] = Form.useForm();
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    useEffect(() => {
+        if (dataUpdate && dataUpdate._id) {
+            form.setFieldsValue({
+                id: dataUpdate._id,
+                mainText: dataUpdate.mainText,
+                author: dataUpdate.author,
+                price: dataUpdate.price,
+                quantity: dataUpdate.quantity,
+                category: dataUpdate.category
+            });
+            setPreview(`${import.meta.env.VITE_URL_BACKEND}/images/book/${dataUpdate.thumbnail}`);
+        }
+    }, [dataUpdate]);
+
+    const updateBook = async (newThumbnail, values) => {
+        const resBook = await updateBookApi(values.id, newThumbnail, values.mainText, values.author, values.price, values.quantity, values.category);
+        if (resBook.data) {
+            handleCancel();
+            await loadBook();
+            notification.success({
+                message: "Success",
+                description: "Update book success!"
+            });
+        } else {
+            notification.error({
+                message: "Error",
+                description: JSON.stringify(resBook.message)
+            });
+        }
+    }
+
+    const handleCancel = () => {
+        form.resetFields();
+        setPreview(null);
+        setDataUpdate(null);
+        setIsModalEditOpen(false);
+    }
+
+    const handleOnChangeFile = (event) => {
+        if (!event.target.files || !event.target.files.length === 0) {
+            setSelectedFile(null);
+            setPreview(`${import.meta.env.VITE_URL_BACKEND}/images/book/${dataUpdate.thumbnail}`);
+        }
+
+        const fileUpload = event.target.files[0];
+        setSelectedFile(fileUpload);
+        setPreview(URL.createObjectURL(fileUpload));
+    }
+
+    const onFinish = async (values) => {
+        if (!preview && !selectedFile) {
+            notification.error({
+                message: "Error",
+                description: "Please upload image files"
+            });
+            return;
+        }
+
+        // có preview + không có file => không upload file
+        let newThumbnail = dataUpdate.thumbnail;
+        // có preview + có file => upload file
+        if (preview && selectedFile) {
+            const resUpload = await handleUploadFile(selectedFile, "book");
+            if (resUpload.data) {
+                newThumbnail = resUpload.data.fileUploaded;
+            } else {
+                notification.error({
+                    message: "Error",
+                    description: JSON.stringify(resUpload.message)
+                });
+                return;
+            }
+        }
+
+        // Update book
+        await updateBook(newThumbnail, values);
+    }
+
+
+    return (
+        <>
+            <Modal
+                title="Update Book (UnControl)"
+                open={isModalEditOpen}
+                onOk={() => form.submit()}
+                onCancel={handleCancel}
+                maskClosable={false}
+                okText="UPDATE"
+            >
+                <Form
+                    form={form}
+                    layout="vertical"
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        label="Id"
+                        name="id"
+                    >
+                        <Input disabled />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Tiêu đề"
+                        name="mainText"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your mainText!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Tác giả"
+                        name="author"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your author!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Giá"
+                        name="price"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your price!',
+                            },
+                        ]}
+                    >
+                        <InputNumber addonAfter="đ" style={{ width: "100%" }} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Số lượng"
+                        name="quantity"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your quantity!',
+                            },
+                        ]}
+                    >
+                        <InputNumber style={{ width: "100%" }} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Thể loại"
+                        name="category"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your mainText!',
+                            },
+                        ]}
+                    >
+                        <Select
+                            placeholder="Please select"
+                            style={{ width: "100%" }}
+                            options={[
+                                { value: 'Arts', label: 'Arts' },
+                                { value: 'Business', label: 'Business' },
+                                { value: 'Comics', label: 'Comics' },
+                                { value: 'Cooking', label: 'Cooking' },
+                                { value: 'Entertainment', label: 'Entertainment' },
+                                { value: 'History', label: 'History' },
+                                { value: 'Music', label: 'Music' },
+                                { value: 'Sports', label: 'Sports' },
+                                { value: 'Teen', label: 'Teen' },
+                                { value: 'Travel', label: 'Travel' },
+                            ]}
+                        />
+                    </Form.Item>
+
+                    <div>
+                        <span>Ảnh thumbnail : </span>
+                        {preview &&
+                            <>
+                                <div style={{
+                                    height: "100px",
+                                    width: "150px",
+                                    marginTop: "10px",
+                                    borderRadius: "5px",
+                                    border: "1px solid #ccc",
+                                    padding: "16px 0"
+                                }}>
+                                    <img
+                                        src={preview}
+                                        alt="Avatar"
+                                        style={{ width: "100%", height: "100%", objectFit: "contain" }}
+                                    />
+                                </div>
+                            </>
+                        }
+
+                        <label
+                            htmlFor="btnUpload"
+                            style={{
+                                display: "block",
+                                width: "fit-content",
+                                background: "#2cae6b",
+                                color: "#fff",
+                                padding: "6px 8px",
+                                borderRadius: "6px",
+                                cursor: "pointer",
+                                margin: "8px 8px 8px 0"
+                            }}
+                        >Upload Image</label>
+
+                        <input
+                            style={{ display: "none" }}
+                            type="file"
+                            accept="image/*"
+                            id="btnUpload"
+                            hidden
+                            onChange={(event) => handleOnChangeFile(event)}
+                            onClick={(event) => event.target.value = null}
+                        />
+                    </div>
+                </Form>
+            </Modal>
+        </>
+    );
+}
+
+
+export default EditBookUnControl;
--- src/components/book/book.table.jsx
+++ src/components/book/book.table.jsx
@@ -6,166 +6,175 @@ import DetailBook from "./book.detail";
 import CreateBook from "./book.create";
 import CreateBookUnControl from "./book.create.uncontrol";
 import EditBook from "./book.edit";
+import EditBookUnControl from "./book.edit.uncontrol";
 
 const BookTable = (props) => {
-  const [dataBooks, setDataBooks] = useState([]);
-  const [current, setCurrent] = useState(1);
-  const [pageSize, setPageSize] = useState(5);
-  const [total, setTotal] = useState(0);
+    const [dataBooks, setDataBooks] = useState([]);
+    const [current, setCurrent] = useState(1);
+    const [pageSize, setPageSize] = useState(5);
+    const [total, setTotal] = useState(0);
 
-  // Detail
-  const [isOpenDetail, setIsOpenDetail] = useState(false);
-  const [dataDetail, setDataDetail] = useState({});
+    // Detail
+    const [isOpenDetail, setIsOpenDetail] = useState(false);
+    const [dataDetail, setDataDetail] = useState({});
 
-  // Create
-  const [isModalCreateOpen, setIsModalCreateOpen] = useState(false);
+    // Create
+    const [isModalCreateOpen, setIsModalCreateOpen] = useState(false);
 
-  // Update
-  const [dataUpdate, setDataUpdate] = useState(null);
-  const [isModalEditOpen, setIsModalEditOpen] = useState(false);
+    // Update
+    const [dataUpdate, setDataUpdate] = useState(null);
+    const [isModalEditOpen, setIsModalEditOpen] = useState(false);
 
-  useEffect(() => {
-    loadBook();
-  }, [current, pageSize]);
+    useEffect(() => {
+        loadBook();
+    }, [current, pageSize]);
 
-  const loadBook = async () => {
-    const res = await fetchAllBookApi(current, pageSize);
-    if (res.data) {
-      setDataBooks(res.data.result);
-      setCurrent(res.data.meta.current)
-      setPageSize(res.data.meta.pageSize)
-      setTotal(res.data.meta.total)
+    const loadBook = async () => {
+        const res = await fetchAllBookApi(current, pageSize);
+        if (res.data) {
+            setDataBooks(res.data.result);
+            setCurrent(res.data.meta.current)
+            setPageSize(res.data.meta.pageSize)
+            setTotal(res.data.meta.total)
+        }
     }
-  }
 
-  // Handle the click event to see Details
-  const handleClickDetail = (data) => {
-    setDataDetail(data);
-    setIsOpenDetail(true);
-  }
+    // Handle the click event to see Details
+    const handleClickDetail = (data) => {
+        setDataDetail(data);
+        setIsOpenDetail(true);
+    }
 
-  // Handle the click event to open the create modal
-  const showModal = () => {
-    setIsModalCreateOpen(true);
-  }
+    // Handle the click event to open the create modal
+    const showModal = () => {
+        setIsModalCreateOpen(true);
+    }
 
-  const columns = [
-    {
-      title: "STT",
-      render: (_, record, index) => <p>{(index + 1) + (current - 1) * pageSize}</p>
-    },
-    {
-      title: 'id',
-      dataIndex: '_id',
-      render: (text, record) => <a onClick={() => handleClickDetail(record)}
-      >{text}</a>,
-    },
-    {
-      title: 'Tiêu đề',
-      dataIndex: 'mainText',
-    },
-    {
-      title: 'Giá tiền',
-      dataIndex: 'price',
-      render: (text, record, index, action) => {
-        if (text)
-          return new Intl.NumberFormat('vi-VN',
-            { style: 'currency', currency: 'VND' }).format(text);
-      }
-    },
-    {
-      title: "Số lượng",
-      dataIndex: "quantity"
-    },
-    {
-      title: 'Tác giả',
-      dataIndex: 'author',
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (_, record) => {
-        return (
-          <>
-            <div style={{ display: "flex", gap: "20px" }}>
-              <EditOutlined
-                style={{
-                  color: "orange",
-                  cursor: "pointer",
-                  fontSize: "18px"
-                }}
-                onClick={() => {
-                  setIsModalEditOpen(true)
-                  setDataUpdate(record);
-                }}
-              />
-              <DeleteOutlined />
+    const columns = [
+        {
+            title: "STT",
+            render: (_, record, index) => <p>{(index + 1) + (current - 1) * pageSize}</p>
+        },
+        {
+            title: 'id',
+            dataIndex: '_id',
+            render: (text, record) => <a onClick={() => handleClickDetail(record)}
+            >{text}</a>,
+        },
+        {
+            title: 'Tiêu đề',
+            dataIndex: 'mainText',
+        },
+        {
+            title: 'Giá tiền',
+            dataIndex: 'price',
+            render: (text, record, index, action) => {
+                if (text)
+                    return new Intl.NumberFormat('vi-VN',
+                        { style: 'currency', currency: 'VND' }).format(text);
+            }
+        },
+        {
+            title: "Số lượng",
+            dataIndex: "quantity"
+        },
+        {
+            title: 'Tác giả',
+            dataIndex: 'author',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => {
+                return (
+                    <>
+                        <div style={{ display: "flex", gap: "20px" }}>
+                            <EditOutlined
+                                style={{
+                                    color: "orange",
+                                    cursor: "pointer",
+                                    fontSize: "18px"
+                                }}
+                                onClick={() => {
+                                    setIsModalEditOpen(true)
+                                    setDataUpdate(record);
+                                }}
+                            />
+                            <DeleteOutlined />
+                        </div>
+                    </>
+                )
+            },
+        },
+    ];
+
+    const onChange = (pagination, filters, sorter, extra) => {
+        if (pagination && pagination.current) {
+            if (+pagination.current !== current) {
+                setCurrent(+pagination.current);
+            }
+        }
+
+        if (pagination && pagination.pageSize) {
+            if (+pagination.pageSize !== pageSize) {
+                setPageSize(+pagination.pageSize);
+            }
+        }
+    }
+
+    return (
+        <>
+            <div style={{ display: "flex", justifyContent: "space-between", margin: "20px 0" }}>
+                <h3>Table book</h3>
+                <Button type="primary" onClick={showModal}> Create</Button>
             </div>
-          </>
-        )
-      },
-    },
-  ];
-
-  const onChange = (pagination, filters, sorter, extra) => {
-    if (pagination && pagination.current) {
-      if (+pagination.current !== current) {
-        setCurrent(+pagination.current);
-      }
-    }
-
-    if (pagination && pagination.pageSize) {
-      if (+pagination.pageSize !== pageSize) {
-        setPageSize(+pagination.pageSize);
-      }
-    }
-  }
-
-  return (
-    <>
-      <div style={{ display: "flex", justifyContent: "space-between", margin: "20px 0" }}>
-        <h3>Table book</h3>
-        <Button type="primary" onClick={showModal}> Create</Button>
-      </div>
-      <Table
-        columns={columns}
-        dataSource={dataBooks}
-        rowKey={"_id"}
-        pagination={{
-          current: current,
-          pageSize: pageSize,
-          total: total,
-          showSizeChanger: true,
-          showTotal: (total, range) => { return (<div> {range[0]}-{range[1]} trên {total} rows</div>) },
-        }}
-        onChange={onChange}
-      />
-      <DetailBook
-        isOpenDetail={isOpenDetail}
-        setIsOpenDetail={setIsOpenDetail}
-        dataDetail={dataDetail}
-        setDataDetail={setDataBooks}
-      />
-      {/* <CreateBook
+            <Table
+                columns={columns}
+                dataSource={dataBooks}
+                rowKey={"_id"}
+                pagination={{
+                    current: current,
+                    pageSize: pageSize,
+                    total: total,
+                    showSizeChanger: true,
+                    showTotal: (total, range) => { return (<div> {range[0]}-{range[1]} trên {total} rows</div>) },
+                }}
+                onChange={onChange}
+            />
+            <DetailBook
+                isOpenDetail={isOpenDetail}
+                setIsOpenDetail={setIsOpenDetail}
+                dataDetail={dataDetail}
+                setDataDetail={setDataBooks}
+            />
+            {/* <CreateBook
         loadBook={loadBook}
         isModalCreateOpen={isModalCreateOpen}
         setIsModalCreateOpen={setIsModalCreateOpen}
       /> */}
 
-      <CreateBookUnControl
-        loadBook={loadBook}
-        isModalCreateOpen={isModalCreateOpen}
-        setIsModalCreateOpen={setIsModalCreateOpen}
-      />
-      <EditBook
+            <CreateBookUnControl
+                loadBook={loadBook}
+                isModalCreateOpen={isModalCreateOpen}
+                setIsModalCreateOpen={setIsModalCreateOpen}
+            />
+            {/* <EditBook
         loadBook={loadBook}
         dataUpdate={dataUpdate}
         setDataUpdate={setDataUpdate}
         isModalEditOpen={isModalEditOpen}
         setIsModalEditOpen={setIsModalEditOpen}
-      />
-    </>
-  );
+      /> */}
+
+            <EditBookUnControl
+                loadBook={loadBook}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+                isModalEditOpen={isModalEditOpen}
+                setIsModalEditOpen={setIsModalEditOpen}
+            />
+        </>
+    );
 }
 
 export default BookTable;
